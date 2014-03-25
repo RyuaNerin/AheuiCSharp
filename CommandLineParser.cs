ac04cbb6a9040ce39c6524bc914b843913be7c40
RyuaNerin <github@ryuanerin.kr>
2014-03-25 22:33:49 +0900

    커멘드 라인 기능 추가

---
+++ CommandLineParser.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RyuaNerin
+{
+	public class CommandLineParser
+	{
+		private Dictionary<string, int>		dicOptionKey;
+		private List<string>				dicOptionDefaultValue;
+
+		private Dictionary<int, string>		dicParsedOptionValue;
+		private List<int>					dicParsedOption;
+		private List<string>				dicParsedCommand;
+
+		public CommandLineParser()
+		{
+			this.dicOptionKey			= new Dictionary<string, int>();
+			this.dicOptionDefaultValue	= new List<string>();
+
+			this.dicParsedOptionValue	= new Dictionary<int, string>();
+			this.dicParsedOption		= new List<int>();
+			this.dicParsedCommand		= new List<string>();
+
+			this.isParsed = false;
+		}
+
+		public void AddOptions(params string[] key)
+		{
+			this.dicOptionDefaultValue.Add(null);
+
+			int index = this.dicOptionDefaultValue.Count - 1;
+
+			for (int i = 0; i < key.Length; i++)
+				this.dicOptionKey.Add(key[i], index);
+		}
+		public void AddOptionsDefault(string defaultValue, params string[] key)
+		{
+			this.dicOptionDefaultValue.Add(defaultValue);
+
+			int index = this.dicOptionDefaultValue.Count - 1;
+
+			for (int i = 0; i < key.Length; i++)
+				this.dicOptionKey.Add(key[i], index);
+		}
+
+		public bool GetOption(string key)
+		{
+			if (!this.dicOptionKey.ContainsKey(key))
+				return false;
+
+			return this.dicParsedOption.Contains(this.dicOptionKey[key]);
+		}
+
+		public string GetOptionValue(string key)
+		{
+			if (this.dicOptionKey.ContainsKey(key))
+			{
+				int index = this.dicOptionKey[key];
+
+				if (this.dicParsedOptionValue.ContainsKey(index))
+					return this.dicParsedOptionValue[index];
+				else
+					return this.dicOptionDefaultValue[index];
+			}
+			else
+				return null;
+		}
+		public string GetOptionDefaultValue(string key)
+		{
+			if (this.dicOptionKey.ContainsKey(key))
+			{
+				int index = this.dicOptionKey[key];
+				
+				return this.dicOptionDefaultValue[index];
+			}
+			else
+				return null;
+		}
+
+		public string[] GetCommand()
+		{
+			return this.dicParsedCommand.ToArray();
+		}
+		public string GetCommand(int index)
+		{
+			if ((index < 0) || (index > this.dicParsedCommand.Count))
+				return null;
+
+			return this.dicParsedCommand[index];
+		}
+		public int CommandLength
+		{
+			get { return this.dicParsedCommand.Count; }
+		}
+
+		public bool isParsed { get; internal set; }
+
+		public bool Parse(string[] args)
+		{
+			try
+			{
+				int i = 0;
+
+				while (i < args.Length)
+				{
+					if (this.dicOptionKey.ContainsKey(args[i]))
+					{
+						int index = this.dicOptionKey[args[i]];
+
+						if (this.dicOptionDefaultValue[index] == null)
+							this.dicParsedOption.Add(index);
+						else
+							this.dicParsedOptionValue.Add(index, args[i + 1]);
+
+						i += 2;
+					}
+					else
+					{
+						this.dicParsedCommand.Add(args[i]);
+						i++;
+					}
+				}
+
+				this.isParsed = true;
+				return true;	
+			}
+			catch
+			{
+				this.isParsed = false;
+				return false;
+			}
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -4,29 +4,124 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
-using RyuaNerin;
 using System.Data;
 
-namespace twitcalc
+using RyuaNerin;
+using System.Text.RegularExpressions;
+
+namespace AheuiCSharp
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static void PrintHelp()
 		{
-			RyuaNerin.Aheui au = new RyuaNerin.Aheui();
+			Console.WriteLine("Usage: {0} <filename> [options]", Regex.Match("/([^\"]+)\"", Environment.CommandLine.Replace('\\', '/')).Groups[1].Value);
+			Console.WriteLine("--help,     -h  : Prints help message");
+			Console.WriteLine("--version,  -v  : Prints Version Information");
+			Console.WriteLine("--stackmin, -sn : Set default stack size ( default : 64 )");
+			Console.WriteLine("--stackmax, -sx : Set maximum stack size ( default : 4096 )");
+			Console.WriteLine("--encoding, -e  : Set the encoding of source code (default :  utf-8 )");
+			Console.WriteLine("    ascii, utf-7, utf-8, utf-16, utf-32, unicode");
+			Console.WriteLine();
+			Console.WriteLine("Made by RyuaNerin, 2014");
+			Console.WriteLine();
+		}
 
-			au.init("밤밣따빠밣밟따빠맣받밪밬따딴박다닥빠맣밠당빠빱맣맣받닫빠맣파빨받밤따다맣맣빠빠밣다맣맣빠밬다맣밬탕빠맣밣타맣발다밤타맣박발땋맣희");
+		static void PrintVersion()
+		{
+			Console.WriteLine("Aheui C# v 1.0.0");
+			Console.WriteLine("Made by RyuaNerin");
+		}
+
+		static int Main(string[] args)
+		{
+			if (args.Length == 0)
+			{
+				PrintHelp();
+				return 0;
+			}
+
+			CommandLineParser parser = new CommandLineParser();
+			parser.AddOptions("--help", "-h");
+			parser.AddOptions("--version", "-v");
+			parser.AddOptionsDefault("utf-8", "--encoding", "-e");
+			parser.AddOptionsDefault("64", "--stackmin", "-sn");
+			parser.AddOptionsDefault("4096", "--stackmax", "-sx");
+
+			if (!parser.Parse(args))
+			{
+				PrintHelp();
+				return 0;
+			}
+
+			if (parser.GetOption("--help"))
+			{
+				PrintHelp();
+				return 0;
+			}
+
+			if (parser.GetOption("--version"))
+			{
+				PrintVersion();
+				return 0;
+			}
+
+			Encoding encoding;
+
+			switch (parser.GetOptionValue("--encoding").ToLower())
+			{
+				case "ascii":
+					encoding = Encoding.ASCII;
+					break;
+				case "utf-7":
+					encoding = Encoding.UTF7;
+					break;
+				case "utf-8":
+					encoding = Encoding.UTF8;
+					break;
+				case "utf-16":
+					encoding = Encoding.BigEndianUnicode;
+					break;
+				case "utf-32":
+					encoding = Encoding.UTF32;
+					break;
+				case "unicode":
+					encoding = Encoding.Unicode;
+					break;
+				default:
+					PrintVersion();
+					return 1;
+			}
+
+			RyuaNerin.Aheui au = new RyuaNerin.Aheui();
+			au.init(File.ReadAllText(parser.GetCommand(0), encoding));
+
+			int i;
+
+			if (!int.TryParse(parser.GetOptionValue("--stackmin"), out i))
+			{
+				PrintVersion();
+				return 1;
+			}
+			au.StackMinSize = (i / 4);
+
+			if (!int.TryParse(parser.GetOptionValue("--stackmax"), out i))
+			{
+				PrintVersion();
+				return 1;
+			}
+			au.StackMaxSize = (i / 4);
 
 			while (au.isRunning)
 			{
-				if (au.state == Aheui.State.WAITING_CHAR)
+				if (au.state == RyuaNerin.Aheui.State.WAITING_CHAR)
 				{
 					Console.WriteLine(au.GetResult());
 					Console.Write("Input Char : ");
 					au.SetInput(Console.ReadLine());
 					Console.WriteLine("");
 				}
-				else if (au.state == Aheui.State.WAITING_NUMBER)
+				else if (au.state == RyuaNerin.Aheui.State.WAITING_NUMBER)
 				{
 					Console.Write("Input Number : ");
 					Console.Write(au.GetResult());
@@ -37,7 +132,8 @@ namespace twitcalc
 			}
 
 			Console.WriteLine(au.GetResult());
-			Console.ReadKey();
+
+			return 0;
 		}
 	}
 }
